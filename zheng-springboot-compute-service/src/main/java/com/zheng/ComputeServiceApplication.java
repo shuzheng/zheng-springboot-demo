6bddba00fdf0a30cd48e834ade5c7989bb9ec672
shuzheng <469741414@qq.com>
2016-11-23 18:02:19 +0800

    add zheng-springboot-compute-service

---
+++ zheng-springboot-compute-service/src/main/java/com/zheng/ComputeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.zheng;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+	}
+}
---
+++ zheng-springboot-compute-service/src/main/java/com/zheng/web/ComputeController.java
@@ -0,0 +1,31 @@
+package com.zheng.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ZhangShuzheng on 2016/11/23.
+ */
+@RestController
+public class ComputeController {
+
+	private final Logger logger = Logger.getLogger(getClass());
+
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+		ServiceInstance instance = client.getLocalServiceInstance();
+		Integer r = a + b;
+		logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+		return r;
+	}
+
+}
