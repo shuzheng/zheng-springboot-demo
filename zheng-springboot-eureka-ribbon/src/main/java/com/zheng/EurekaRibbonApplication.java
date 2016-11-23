d6bf8e25b42f66b2e15ace4826d4d1a21c9e8112
shuzheng <469741414@qq.com>
2016-11-23 18:02:33 +0800

    add zheng-springboot-eureka-ribbon

---
+++ zheng-springboot-eureka-ribbon/src/main/java/com/zheng/EurekaRibbonApplication.java
@@ -0,0 +1,23 @@
+package com.zheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaRibbonApplication {
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRibbonApplication.class, args);
+	}
+}
