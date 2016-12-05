41331c19ca3c3294f51b59a31a50c7181213c7fa
shuzheng <469741414@qq.com>
2016-12-05 14:29:51 +0800

    统一异常

---
+++ springboot/src/main/java/com/zheng/springboot/exception/MyException.java
@@ -0,0 +1,12 @@
+package com.zheng.springboot.exception;
+
+/**
+ * Created by ZhangShuzheng on 2016/12/5.
+ */
+public class MyException extends Exception {
+
+	public MyException(String message) {
+		super(message);
+	}
+
+}
