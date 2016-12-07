377981af59d3701c8cb3d80aa5b94089441bd993
shuzheng <469741414@qq.com>
2016-12-07 18:00:25 +0800

    分布式配置走注册中心

--- zheng-springboot-config-client/src/main/java/com/zheng/ConfigClientApplication.java
+++ zheng-springboot-config-client/src/main/java/com/zheng/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.zheng;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
--- zheng-springboot-config-server/src/main/java/com/zheng/ConfigServerApplication.java
+++ zheng-springboot-config-server/src/main/java/com/zheng/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.zheng;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
